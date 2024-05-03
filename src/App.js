9906113635979ca8ba651c934ba35256bb19a476
Daniil Matyash <daniil.matyash@aisa.ru>
2024-05-03 13:11:33 +0300

    add quiz

--- src/App.js
+++ src/App.js
@@ -1,29 +1,87 @@
 import { useState } from "react"
 import "./index.scss"
 
+const questions = [
+  {
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
+    correct: 0,
+  },
+  {
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
+    correct: 1,
+  },
+  {
+    title: "Что такое JSX?",
+    variants: [
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
+    ],
+    correct: 2,
+  },
+]
+
+function Result({ correct }) {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
+      <a href="/">
+        <button>Попробовать снова</button>
+      </a>
+    </div>
+  )
+}
+
+function Game({ question, step, onVariantClick }) {
+  const percentage = Math.round((step / questions.length) * 100)
+  return (
+    <>
+      <div className="progress">
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress__inner"
+        ></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((text, index) => (
+          <li key={index} onClick={() => onVariantClick(index)}>
+            {text}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
 function App() {
-  let [count, setCount] = useState(0)
+  const [step, setStep] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const question = questions[step]
 
-  function onClickPlus() {
-    setCount(count + 1)
-  }
-
-  function onClickMinus() {
-    setCount(count - 1)
+  function onVariantClick(index) {
+    if (question.correct === index) {
+      setCorrect(correct + 1)
+    }
+    setStep(step + 1)
   }
 
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button onClick={onClickMinus} className="minus">
-          - Минус
-        </button>
-        <button onClick={onClickPlus} className="plus">
-          Плюс +
-        </button>
-      </div>
+      {step !== questions.length ? (
+        <Game question={question} step={step} onVariantClick={onVariantClick} />
+      ) : (
+        <Result correct={correct} />
+      )}
     </div>
   )
 }
