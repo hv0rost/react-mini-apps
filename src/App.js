2820cd9bc235ea9f7ad6ee03ba8c80bc678d604e
Daniil Matyash <daniil.matyash@aisa.ru>
2024-05-03 12:45:33 +0300

    Добавлен диалог

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
 import { useState } from "react"
 import "./index.scss"
 
+function Dialog({ dialog, setDialog, children }) {
+  return (
+    <div className={dialog ? "overlay show" : "overlay animated"}>
+      <div className="modal">
+        <svg
+          height="200"
+          viewBox="0 0 200 200"
+          width="200"
+          onClick={() => setDialog(false)}
+        >
+          <title />
+          <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+        </svg>
+        {children}
+      </div>
+    </div>
+  )
+}
+
 function App() {
-  let [count, setCount] = useState(0)
-
-  function onClickPlus() {
-    setCount(count + 1)
-  }
-
-  function onClickMinus() {
-    setCount(count - 1)
-  }
+  const [dialog, setDialog] = useState(false)
 
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button onClick={onClickMinus} className="minus">
-          - Минус
-        </button>
-        <button onClick={onClickPlus} className="plus">
-          Плюс +
-        </button>
-      </div>
+      <button onClick={() => setDialog(true)} className="open-modal-btn">
+        ✨ Открыть окно
+      </button>
+
+      {/* {dialog && (
+        <div className="overlay">
+          <div className="modal">
+            <svg
+              height="200"
+              viewBox="0 0 200 200"
+              width="200"
+              onClick={() => setDialog(false)}
+            >
+              <title />
+              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            </svg>
+            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+          </div>
+        </div>
+      )} */}
+      <Dialog dialog={dialog} setDialog={setDialog}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+      </Dialog>
     </div>
   )
 }
