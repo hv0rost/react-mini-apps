6d56dda1140628a4fb6deb797d32a14711ca7541
Daniil Matyash <daniil.matyash@aisa.ru>
2024-05-08 16:56:07 +0300

    Add photo app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,82 @@
-import { useState } from "react"
 import "./index.scss"
+import { data } from "./assets/data.js"
 
-function App() {
-  let [count, setCount] = useState(0)
+import React, { useEffect, useState } from "react"
 
-  function onClickPlus() {
-    setCount(count + 1)
-  }
+import Collection from "./components/Collection"
 
-  function onClickMinus() {
-    setCount(count - 1)
-  }
+export default function App() {
+  const categories = ["Все", "Море", "Горы", "Архитектура", "Города"]
+  const [filterValue, setFilterValue] = useState("")
+  const [selectedCategory, setSelectedCategory] = useState(0)
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {}, [])
 
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button onClick={onClickMinus} className="minus">
-          - Минус
-        </button>
-        <button onClick={onClickPlus} className="plus">
-          Плюс +
-        </button>
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          {categories.map((category, index) => (
+            <li
+              key={index}
+              onClick={() => setSelectedCategory(index)}
+              className={selectedCategory === index ? "active" : ""}
+            >
+              {category}
+            </li>
+          ))}
+        </ul>
+        <input
+          value={filterValue}
+          onChange={(e) => setFilterValue(e.target.value)}
+          className="search-input"
+          placeholder="Поиск по названию"
+        />
       </div>
+      <div className="content">
+        {data.collections
+          .filter(
+            (item) =>
+              selectedCategory === 0 || item.category === selectedCategory
+          )
+          .filter((item) =>
+            item.name.toLowerCase().includes(filterValue.toLowerCase())
+          )
+          .slice((page - 1) * 4, page * 4)
+          .map((collection, index) => (
+            <Collection
+              key={index}
+              name={collection.name}
+              images={collection.photos}
+            />
+          ))}
+      </div>
+      <ul className="pagination">
+        {[
+          ...Array(
+            Math.round(
+              data.collections
+                .filter(
+                  (item) =>
+                    selectedCategory === 0 || item.category === selectedCategory
+                )
+                .filter((item) =>
+                  item.name.toLowerCase().includes(filterValue.toLowerCase())
+                ).length / 4
+            )
+          ),
+        ].map((_, index) => (
+          <li
+            key={index}
+            className={page === index + 1 ? "active" : ""}
+            onClick={() => setPage(index + 1)}
+          >
+            {index + 1}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
-
-export default App
